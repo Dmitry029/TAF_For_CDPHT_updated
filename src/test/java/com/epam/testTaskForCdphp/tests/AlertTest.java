e8a83ef75d6929b0c8dfa9b6e5d1b65f6657b413
Dmitry029 <kdp.029029@gmail.com>
2020-11-27 17:18:00 +0300

    Fo new repo.

--- src/test/java/com/epam/testTaskForCdphp/tests/AlertTest.java
+++ src/test/java/com/epam/testTaskForCdphp/tests/AlertTest.java
@@ -9,7 +9,7 @@ import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
 
 /**
- *  Test 2 realisation.
+ *  Test 2 realisation
  */
 
 public class AlertTest extends TestUtilities {
